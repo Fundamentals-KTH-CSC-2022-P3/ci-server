732f809f64aec319c744af280ff7e7bf1648ab6b
Samuel Ph <>
2022-02-04 15:33:14 +0100

    Feat: Added boiler-plate code, maven pom, README setup

---
+++ src/main/java/fundamentals/server/ContinuousIntegrationServer.java
@@ -0,0 +1,59 @@
+package fundamentals.server;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+
+import java.io.IOException;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+
+public class ContinuousIntegrationServer extends AbstractHandler {
+
+    final static int DEFAULT_PORT_NUMBER = 8014;
+
+    static int getPortNumberFromInputOrElseDefault(String[] args) {
+        try {
+            return Integer.parseInt(args[0]);
+        } catch (NumberFormatException ex) {
+            System.err.println("Unparsable port number");
+            System.out.println("usage: ci-server <port>");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            return DEFAULT_PORT_NUMBER;
+        }
+        return DEFAULT_PORT_NUMBER;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting up server...");
+        var portNumber = getPortNumberFromInputOrElseDefault(args);
+        var server = new Server(portNumber);
+        server.setHandler(new ContinuousIntegrationServer());
+        server.start();
+        System.out.println("Server has successfully started on port " + portNumber);
+        try {
+            server.join();
+        } catch (InterruptedException ex) {
+            System.err.println("Interrupted, gracefully shutting down...");
+            throw ex;
+        }
+    }
+
+    @Override
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+
+        System.out.println(target);
+
+        response.getWriter().println("CI job done");
+    }
+}

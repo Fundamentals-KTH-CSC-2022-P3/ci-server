6c0a42a079e99285fdba1b327a8c93772c05fe66
SamuSoft <samuelph@kth.se>
2022-02-06 13:35:42 +0100

    Feat: Issue #1: Add boilerplate code and maven (#12)
    * Feat: Added boiler-plate code, maven pom, README setup
    
    * Bug: Changed non-rendered char
    
    Co-authored-by: Samuel Ph <>

---
+++ src/test/java/fundamentals/server/ContinuousIntegrationServerTest.java
@@ -0,0 +1,32 @@
+package fundamentals.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContinuousIntegrationServerTest {
+
+    @Test
+    void getPortNumberFromInputOrElseDefaultTestWithValidInput() {
+        var portNumber = 123456;
+        var arrayWithPort = new String[]{String.valueOf(portNumber)};
+        var parsedPortNumber = ContinuousIntegrationServer.getPortNumberFromInputOrElseDefault(arrayWithPort);
+        assertEquals(portNumber, parsedPortNumber);
+    }
+
+    @Test
+    void getPortNumberFromInputOrElseDefaultTestWithInvalidInput() {
+        var portNumber = ContinuousIntegrationServer.DEFAULT_PORT_NUMBER;
+        var arrayWithPort = new String[]{"Hello", "World"};
+        var returnedDefault = ContinuousIntegrationServer.getPortNumberFromInputOrElseDefault(arrayWithPort);
+        assertEquals(portNumber, returnedDefault);
+    }
+
+    @Test
+    void getPortNumberFromInputOrElseDefaultTestWithNoInput() {
+        var portNumber = ContinuousIntegrationServer.DEFAULT_PORT_NUMBER;
+        var arrayWithPort = new String[0];
+        var returnedDefault = ContinuousIntegrationServer.getPortNumberFromInputOrElseDefault(arrayWithPort);
+        assertEquals(portNumber, returnedDefault);
+    }
+}

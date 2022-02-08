89a2644a19357bbbb5ef740710e1826a7f9840c8
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-02-08 12:57:14 +0100

    add builds.json to .gitignore

--- src/main/java/fundamentals/server/BuildStorage.java
+++ src/main/java/fundamentals/server/BuildStorage.java
@@ -2,9 +2,7 @@ package fundamentals.server;
 
 import org.json.JSONArray;
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 public class BuildStorage {
 
@@ -35,7 +33,11 @@ public class BuildStorage {
             instance.builds = new JSONArray(jsonText.toString());
         } catch (FileNotFoundException e) {
             // Builds file does not exist. Create a builds.json file with an empty array inside.
-            
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(BUILD_STORAGE_FILE, StandardCharsets.UTF_8))){
+                writer.write("[]");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }

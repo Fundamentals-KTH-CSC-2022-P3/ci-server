ef2cb44dfede0d2c9a9eff4033c8984c705f6d99
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-02-08 12:50:17 +0100

    work on the builds storage

---
+++ src/main/java/fundamentals/server/BuildStorage.java
@@ -0,0 +1,43 @@
+package fundamentals.server;
+
+import org.json.JSONArray;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+public class BuildStorage {
+
+    public static final String BUILD_STORAGE_FILE = "builds.json";
+
+    private static BuildStorage instance;
+
+    private JSONArray builds;
+
+    public static BuildStorage getInstance() {
+        if (instance == null) {
+            loadBuildStorageFile();
+        }
+
+        return instance;
+    }
+
+    private static void loadBuildStorageFile() {
+        instance = new BuildStorage();
+
+        // Read the builds from disk into main-memory.
+        try (BufferedReader reader = new BufferedReader(new FileReader(BUILD_STORAGE_FILE, StandardCharsets.UTF_8))) {
+            StringBuilder jsonText = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonText.append(line);
+            }
+            instance.builds = new JSONArray(jsonText.toString());
+        } catch (FileNotFoundException e) {
+            // Builds file does not exist. Create a builds.json file with an empty array inside.
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

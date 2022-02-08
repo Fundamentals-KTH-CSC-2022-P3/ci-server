f0f1d2c19d31b8aa580c1e3d582bb9145e5cf4e3
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-02-08 09:51:51 +0100

    add handlers for each endpoint

---
+++ src/main/java/fundamentals/server/BuildAllHandler.java
@@ -0,0 +1,27 @@
+package fundamentals.server;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import java.io.IOException;
+
+public class BuildAllHandler extends AbstractHandler {
+
+    @Override
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+
+        System.out.println("build ALL handler");
+
+        response.getWriter().println("CI job done");
+    }
+}
--- src/main/java/fundamentals/server/ContinuousIntegrationServer.java
+++ src/main/java/fundamentals/server/ContinuousIntegrationServer.java
@@ -1,16 +1,12 @@
 package fundamentals.server;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.ServletException;
 
-import java.io.IOException;
-
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 
 
-public class ContinuousIntegrationServer extends AbstractHandler {
+public class ContinuousIntegrationServer {
 
     final static int DEFAULT_PORT_NUMBER = 8014;
 
@@ -26,12 +22,26 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         return DEFAULT_PORT_NUMBER;
     }
 
+    static ContextHandler getContextHandler(String path, Handler handler) {
+        var context = new ContextHandler();
+        context.setContextPath(path);
+        context.setHandler(handler);
+        return context;
+    }
+
+    static ContextHandlerCollection getEndpointsHandler() {
+        var endpoints = new ContextHandlerCollection();
+        endpoints.addHandler(getContextHandler("/webhook", new WebhookHandler()));
+        endpoints.addHandler(getContextHandler("/build/all", new BuildAllHandler()));
+        endpoints.addHandler(getContextHandler("/build", new BuildHandler()));
+        return endpoints;
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting up server...");
         var portNumber = getPortNumberFromInputOrElseDefault(args);
         var server = new Server(portNumber);
-        server.setHandler(new ContinuousIntegrationServer());
+        server.setHandler(getEndpointsHandler());
         server.start();
         System.out.println("Server has successfully started on port " + portNumber);
         try {
@@ -40,20 +50,5 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             System.err.println("Interrupted, gracefully shutting down...");
             throw ex;
         }
-    }
-
-    @Override
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
-            throws IOException, ServletException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-
-        System.out.println(target);
-
-        response.getWriter().println("CI job done");
     }
 }

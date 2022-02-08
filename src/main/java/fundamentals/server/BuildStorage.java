a6ae0c01b9a1365bf4aad1ad0626de078a93b122
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-02-08 19:12:16 +0100

    update

--- src/main/java/fundamentals/server/BuildStorage.java
+++ src/main/java/fundamentals/server/BuildStorage.java
@@ -1,8 +1,13 @@
 package fundamentals.server;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class BuildStorage {
 
@@ -40,6 +45,80 @@ public class BuildStorage {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public JSONObject addNewBuild(String commitSha, String repository, String owner) {
+        JSONObject build = new JSONObject();
+        build.put("build_id", UUID.randomUUID());
+        build.put("commit", commitSha);
+        build.put("repository", repository);
+        build.put("owner", owner);
+        build.put("build_started", Instant.now().toString());
+        build.put("compile_status", "pending");
+        build.put("test_status", "pending");
+        build.put("compile_logs", new JSONArray());
+        build.put("test_logs", new JSONArray());
+        builds.put(build);
+        return build;
+    }
+
+    public JSONObject findBuild(String buildID) {
+        // A good idea is probably to keep an index that maps build_id to position in array.
+        // Then we don't have to do a linear search each time.
+        for (int i = 0; i < builds.length(); i++) {
+            JSONObject build = builds.getJSONObject(i);
+            if (build.getString("build_id").equals(buildID)) {
+                return build;
+            }
+        }
+        // Could not find a build with the requested build ID.
+        return null;
+    }
+
+    public boolean setBuildCompileStatus(String buildID, String status) {
+        return setBuildValue(buildID, "compile_status", status);
+    }
+
+    public boolean setBuildTestStatus(String buildID, String status) {
+        return setBuildValue(buildID, "test_status", status);
+    }
+
+    public boolean setBuildEndedTime(String buildID) {
+        return setBuildValue(buildID, "build_ended", Instant.now().toString());
+    }
+
+    private boolean setBuildValue(String buildID, String key, String value) {
+        JSONObject build = findBuild(buildID);
+        if (build == null)
+            return false;
+        build.put(key, value);
+        return true;
+    }
+
+    public boolean setBuildCompileLogs(String buildID, ArrayList<String> logs) {
+        return setBuildArrayValues(buildID, "compile_logs", logs);
+    }
+
+    public boolean setBuildTestLogs(String buildID, ArrayList<String> logs) {
+        return setBuildArrayValues(buildID, "test_logs", logs);
+    }
+
+    private boolean setBuildArrayValues(String buildID, String key, ArrayList<String> values) {
+        JSONObject build = findBuild(buildID);
+        if (build == null)
+            return false;
+        JSONArray array = build.getJSONArray(key);
+        for (String value : values)
+            array.put(value);
+        return true;
+    }
+
+    public void saveToDisk() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(BUILD_STORAGE_FILE, StandardCharsets.UTF_8))){
+            writer.write(builds.toString());
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }

32be92e3aaf3273f1b23e8869dfecc5a65ef22dd
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-02-09 07:37:31 +0100

    add unit-tests for the BuildStorage class

--- src/main/java/fundamentals/server/BuildStorage.java
+++ src/main/java/fundamentals/server/BuildStorage.java
@@ -11,7 +11,8 @@ import java.util.UUID;
 
 /**
  * Represents a persistent storage that will store all builds both in main-memory but also on disk in JSON format.
- * Everytime the server restarts the builds.json file will be loaded into main-memory.
+ * Everytime the server restarts the builds.json file will be loaded into main-memory. Use the {@code saveToDisk}
+ * method when a new build is added to ensure that we keep an updated version of the builds on disk.
  * <p>
  * The {@code BuildStorage} class is implemented using the Singleton pattern which means that the "builds.json" file gets loaded after
  * the method {@code getInstance} has been called for the first time.
@@ -22,10 +23,24 @@ public class BuildStorage {
 
     private static BuildStorage instance;
 
+    // The build ID is a universally unique identifier (UUID).
+    // If we don't want to do a linear search when the user asks for information about a build with a certain build ID,
+    // then we should keep a lookup table that maps each build ID to an array index.
     private HashMap<String, Integer> buildIDToArrayIndex = new HashMap<>();
+
+    // All the builds are stored here.
     private JSONArray builds;
+
+    // The path to the file where we should save all our builds.
     private String filePath;
 
+    /**
+     * Returns an instance of this class. Only one instance of this class will ever be created during program execution.
+     * This method should only be called from unit-tests. If you are not working on unit tests then call the {@code getInstance()}
+     * method instead that will use the default "builds.json" file.
+     *
+     * @return an instance of type {@code BuildStorage}
+     */
     public static BuildStorage getInstance(String filePath) {
         if (instance == null) {
             loadBuildStorageFile(filePath);
@@ -34,10 +49,21 @@ public class BuildStorage {
         return instance;
     }
 
+    /**
+     * Returns an instance of this class. Only one instance of this class will ever be created during program execution.
+     * The instance will store all the builds from the "builds.json" file.
+     *
+     * @return an instance of type {@code BuildStorage}
+     */
     public static BuildStorage getInstance() {
         return getInstance(DEFAULT_BUILD_STORAGE_FILE);
     }
 
+    /**
+     * Creates an instance of the {@code BuildStorage} class and loads the builds file from disk into main-memory.
+     *
+     * @param filePath the path to the builds file.
+     */
     private static void loadBuildStorageFile(String filePath) {
         instance = new BuildStorage();
         instance.filePath = filePath;
@@ -49,17 +75,21 @@ public class BuildStorage {
             while ((line = reader.readLine()) != null) {
                 jsonText.append(line);
             }
+
+            // Parse the JSON text to an actual JSONArray object.
             instance.builds = new JSONArray(jsonText.toString());
 
+            // Create mappings from build ID to array index (this is only done for performance).
             for (int i = 0; i < instance.builds.length(); i++) {
                 JSONObject build = instance.builds.getJSONObject(i);
                 String buildID = build.getString("build_id");
                 instance.buildIDToArrayIndex.put(buildID, i);
             }
         } catch (FileNotFoundException e) {
-            // Builds file does not exist. Create a builds.json file with an empty array inside.
+            // Builds file does not exist. Create a builds file with an empty array inside.
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, StandardCharsets.UTF_8))) {
                 writer.write("[]");
+                instance.builds = new JSONArray();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -68,12 +98,23 @@ public class BuildStorage {
         }
     }
 
+    /**
+     * Will add a new build to the array stored in main-memory. However, this call will not update the file on disk.
+     * If you want to update the file on disk then you have to call {@code saveToDisk()} after you have
+     * added your build.
+     *
+     * @param commitHash the commit hash.
+     * @param repository the repository.
+     * @param owner      the owner of the repository.
+     * @return an object of type {@code JSONObject} that contains information about the current build.
+     */
     public JSONObject addNewBuild(String commitHash, String repository, String owner) {
         JSONObject build = new JSONObject();
 
         // Generate a unique identifier for this build.
         String buildID = UUID.randomUUID().toString();
 
+        // The information we will store about each build.
         build.put("build_id", buildID);
         build.put("commit", commitHash);
         build.put("repository", repository);
@@ -85,12 +126,28 @@ public class BuildStorage {
         build.put("compile_logs", new JSONArray());
         build.put("test_logs", new JSONArray());
 
+        // Add the build to the array and also store the mapping from build ID -> array index.
         builds.put(build);
         buildIDToArrayIndex.put(buildID, buildIDToArrayIndex.size() - 1);
 
         return build;
     }
 
+    /**
+     * Returns the number of builds stored in main-memory.
+     *
+     * @return number of builds.
+     */
+    public int size() {
+        return builds.length();
+    }
+
+    /**
+     * Returns a JSON object that contains information about a build given the build ID.
+     *
+     * @param buildID specify which build you want information about using the build ID.
+     * @return an object of type {@code JSONObject} that contains relevant information about the build.
+     */
     public JSONObject getBuild(String buildID) {
         // If we can't find a build with that ID then we will return null.
         if (!buildIDToArrayIndex.containsKey(buildID))
@@ -101,6 +158,9 @@ public class BuildStorage {
         return builds.getJSONObject(index);
     }
 
+    /**
+     * Will write all builds that are stored in main-memory to a file on disk.
+     */
     public void saveToDisk() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, StandardCharsets.UTF_8))) {
             writer.write(builds.toString());
---
+++ src/test/java/fundamentals/server/BuildStorageTest.java
@@ -0,0 +1,73 @@
+package fundamentals.server;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for the {@code BuildStorage} class.
+ */
+public class BuildStorageTest {
+
+    private static final String BUILDS_TEST_FILE = "src/test/res/builds.test.json";
+
+    /**
+     * Ensure that it is possible to retrieve existing builds from the builds file on disk.
+     */
+    @Test
+    @DisplayName("Retrieve existing builds test")
+    void retrieveBuildsTest() {
+        BuildStorage storage = BuildStorage.getInstance(BUILDS_TEST_FILE);
+        assertNotNull(storage.getBuild("9ff81d98-ee33-444c-991c-8005fd6f7b62"));
+        assertNotNull(storage.getBuild("3663086f-4bea-439f-a8d4-c6721b61a28e"));
+        assertNotNull(storage.getBuild("c7c4fec1-a849-4a48-89fd-4d3f343f7e11"));
+    }
+
+    /**
+     * Ensure that it is not possible to retrieve builds that do not exist on disk.
+     */
+    @Test
+    @DisplayName("Cannot retrieve non-existing builds test")
+    void cannotRetrieveNonExistingBuildsTest() {
+        BuildStorage storage = BuildStorage.getInstance(BUILDS_TEST_FILE);
+        assertNull(storage.getBuild("fff81d98-ee33-444c-991c-8005fd6f7b62"));
+        assertNull(storage.getBuild("3663086f-424a-439f-a8d4-c6721b61a28e"));
+        assertNull(storage.getBuild("c7c4fec1-a849-4a48-89fd-4d3f343f7e91"));
+    }
+
+    /**
+     * Ensure that the correct amount of builds is stored in main-memory.
+     */
+    @Test
+    @DisplayName("The correct number of builds are stored test")
+    void correctNumberOfBuildsTest() {
+        BuildStorage storage = BuildStorage.getInstance(BUILDS_TEST_FILE);
+        assertEquals(3, storage.size());
+    }
+
+    /**
+     * Ensure that we can retrieve the correct build information given a valid build ID.
+     */
+    @Test
+    @DisplayName("Can retrieve the correct build information test")
+    void retrieveKeyValuePairsFromEnvironmentTest() {
+        BuildStorage storage = BuildStorage.getInstance(BUILDS_TEST_FILE);
+
+        JSONObject build1 = storage.getBuild("9ff81d98-ee33-444c-991c-8005fd6f7b62");
+        assertEquals("example-owner", build1.getString("owner"));
+        assertEquals("example-hash", build1.getString("commit"));
+        assertEquals("example-repo", build1.getString("repository"));
+
+        JSONObject build2 = storage.getBuild("3663086f-4bea-439f-a8d4-c6721b61a28e");
+        assertEquals("example-owner-2", build2.getString("owner"));
+        assertEquals("example-hash-2", build2.getString("commit"));
+        assertEquals("example-repo-2", build2.getString("repository"));
+
+        JSONObject build3 = storage.getBuild("c7c4fec1-a849-4a48-89fd-4d3f343f7e11");
+        assertEquals("example-owner-3", build3.getString("owner"));
+        assertEquals("example-hash-3", build3.getString("commit"));
+        assertEquals("example-repo-3", build3.getString("repository"));
+    }
+}

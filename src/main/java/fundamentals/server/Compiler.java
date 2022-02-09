ea5cbc4509060dc2fbf7f8394cce849455a3c64f
Arvid Siberov <asiberov@kth.se>
2022-02-09 23:37:31 +0100

    Add unit tests to the Compiler class

--- src/main/java/fundamentals/server/Compiler.java
+++ src/main/java/fundamentals/server/Compiler.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Compiler {
     private final RepoManager repoManager;
-    private final String[] compileCmd = new String[]{"mvn", "compile"};
+    private final String[] compileCmd = new String[]{"mvn.cmd", "compile"};
 
     public Compiler(RepoManager repoManager) {
         this.repoManager = repoManager;
@@ -12,17 +12,19 @@ public class Compiler {
 
     /**
      * Compiles the repository using maven, and returns true if the exit code from "mvn compile" is 0.
-     * @return true if the exit code from maven is 0
+     * @return the return code of "mvn compile"
      * @throws IOException if the specified working directory is unreachable
      * @throws InterruptedException if the compilation process could not complete its execution.
      */
-    public void compile() throws IOException, InterruptedException {
+    public int compile() throws IOException, InterruptedException {
         Process compileProcess = Runtime.getRuntime().exec(compileCmd, null, repoManager.repoDir);
         compileProcess.waitFor();
-        if (compileProcess.exitValue() == 0) {
+        int exitValue = compileProcess.exitValue();
+        if (exitValue == 0) {
             System.out.println("Compilation successful.");
         } else {
             System.out.println("Compilation failed.");
         }
+        return exitValue;
     }
 }
--- src/test/java/fundamentals/server/CompilerTest.java
+++ src/test/java/fundamentals/server/CompilerTest.java
@@ -1,7 +1,50 @@
 package fundamentals.server;
 
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This unit tests the compiler by cloning a remote repo and trying to compile it.
+ * A better way would be to create a minimal Maven repo as part of the test fixture
+ * since that would remove an external dependency. //Arvid
+ */
 public class CompilerTest {
-    /**
-     * How 2 test this? //Arvid
-     */
+    private static final String validPayload = "{\"ref\": \"refs/heads/main\",\"repository\": {\"name\": \"ci-server\",\"clone_url\": \"https://github.com/Fundamentals-KTH-CSC-2022-P3/ci-server.git\",}}\n";
+    private RepoManager repoManager;
+    private File workDir = new File("testWorkDir/");
+    private Compiler compiler;
+
+    @BeforeEach
+    void setup() throws IOException {
+        if (!workDir.mkdirs()) {
+            throw new IOException("Could not create directory " + workDir.getAbsolutePath());
+        }
+        repoManager = new RepoManager(validPayload, workDir);
+        repoManager.cloneRepo();
+        compiler = new Compiler(repoManager);
+    }
+
+    @AfterEach
+    void tearDown() {
+        RepoManager.deleteDirectory(workDir);
+    }
+
+    @Test
+    void compileClonedDirectoryIsSuccessful() throws IOException, InterruptedException {
+        assertEquals(compiler.compile(), 0);
+    }
+
+    @Test
+    void compileDisturbedDirectoryFails() throws IOException, InterruptedException {
+        File pom = new File(repoManager.repoDir, "pom.xml");
+        FileWriter fw = new FileWriter(pom);
+        fw.append("invalid XML");
+        fw.close();
+        assertNotEquals(compiler.compile(), 0);
+    }
 }

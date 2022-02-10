b7288a3aa21ef635032f7859d62bc699846c5d76
Arvid Siberov <asiberov@kth.se>
2022-02-10 10:48:24 +0100

    Undo faulty changes to ContinuousIntegrationServer

--- src/main/java/fundamentals/server/ContinuousIntegrationServer.java
+++ src/main/java/fundamentals/server/ContinuousIntegrationServer.java
@@ -1,18 +1,12 @@
 package fundamentals.server;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
 
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 
 
-public class ContinuousIntegrationServer extends AbstractHandler {
+public class ContinuousIntegrationServer {
 
     final static int DEFAULT_PORT_NUMBER = 8014;
 
@@ -56,40 +50,5 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             System.err.println("Interrupted, gracefully shutting down...");
             throw ex;
         }
-    }
-
-    @Override
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
-            throws IOException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-
-        String payload = "";
-        if (baseRequest.getMethod().equals("POST")) {
-            payload = request.getParameter("payload");
-            if (payload == null)
-                return;
-        }
-        RepoManager repoManager;
-        try {
-            repoManager = new RepoManager(payload);
-        } catch (Exception e) {
-            System.out.println("Got exception!");
-            e.printStackTrace();
-            return;
-        }
-
-        repoManager.cloneRepo();
-        TestRunner testRunner = new TestRunner(repoManager);
-        testRunner.run();
-        repoManager.cleanUp();
-
-        System.out.println(target);
-
-        response.getWriter().println("CI job done");
     }
 }

4ee261d50483c54a75862f9727ec491f98cbe171
Samuel Ph <>
2022-02-10 20:38:53 +0100

    Feat: Added Basic Auth to all but one endpoint

--- src/main/java/fundamentals/server/ContinuousIntegrationServer.java
+++ src/main/java/fundamentals/server/ContinuousIntegrationServer.java
@@ -1,9 +1,18 @@
 package fundamentals.server;
 
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.UserStore;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.security.Credential;
+
+import java.util.Collections;
 
 
 public class ContinuousIntegrationServer {
@@ -24,18 +33,39 @@ public class ContinuousIntegrationServer {
         return DEFAULT_PORT_NUMBER;
     }
 
-    static ContextHandler getContextHandler(String path, Handler handler) {
+    static Handler getContextHandler(String path, Handler handler) {
         var context = new ContextHandler();
         context.setContextPath(path);
         context.setHandler(handler);
         return context;
     }
 
+    static Handler getSecureHandler(String path, Handler handler) {
+        var context = new ContextHandler();
+        context.setContextPath(path);
+        context.setHandler(handler);
+        var security = new ConstraintSecurityHandler();
+        security.setAuthenticator(new BasicAuthenticator());
+
+        var constraint = new Constraint();
+        constraint.setAuthenticate(true);
+        constraint.setRoles(SecurityManager.getInstance().getRoles());
+        var mapping = new ConstraintMapping();
+        mapping.setPathSpec(path);
+        mapping.setConstraint(constraint);
+        security.setConstraintMappings(Collections.singletonList(mapping));
+
+        var loginService = SecurityManager.getInstance().getAdminLoginService();
+        security.setLoginService(loginService);
+        security.setHandler(context);
+        return security;
+    }
+
     static ContextHandlerCollection getEndpointsHandler() {
         var endpoints = new ContextHandlerCollection();
         endpoints.addHandler(getContextHandler("/webhook", new WebhookHandler(environment, storage)));
-        endpoints.addHandler(getContextHandler("/build/all", new BuildAllHandler(storage)));
-        endpoints.addHandler(getContextHandler("/build", new BuildHandler(storage)));
+        endpoints.addHandler(getSecureHandler("/build/all", new BuildAllHandler(storage)));
+        endpoints.addHandler(getSecureHandler("/build", new BuildHandler(storage)));
         return endpoints;
     }
 
@@ -44,6 +74,10 @@ public class ContinuousIntegrationServer {
         var portNumber = getPortNumberFromInputOrElseDefault(args);
         var server = new Server(portNumber);
         server.setHandler(getEndpointsHandler());
+
+        var loginService = SecurityManager.getInstance().getAdminLoginService();
+        server.addBean(loginService);
+
         server.start();
         System.out.println("Server has successfully started on port " + portNumber);
         try {
---
+++ src/main/java/fundamentals/server/SecurityManager.java
@@ -0,0 +1,62 @@
+package fundamentals.server;
+
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.LoginService;
+import org.eclipse.jetty.security.UserStore;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.util.security.Credential;
+
+public class SecurityManager {
+
+    private BasicAuthenticator authenticator;
+    private final static SecurityManager instance = new SecurityManager();
+    private final static String usernameEnvVariableName = "CI_SERVER_USER";
+    private final static String passwordEnvVariableName = "CI_SERVER_PASS";
+    private final static String[] ROLES = new String[]{"admin"};
+    private String username;
+    private Credential password;
+    private HashLoginService adminLoginService = null;
+
+    private SecurityManager() {
+        loadUsername();
+        loadPassword();
+    }
+
+    private void loadUsername(){
+        this.username = Environment.loadEnvironmentVariableOrElse(usernameEnvVariableName, "admin");
+    }
+
+    private void loadPassword(){
+        var password = Environment.loadEnvironmentVariableOrElse(passwordEnvVariableName, "adminpass");
+        this.password = Credential.getCredential(password);
+    }
+
+    public Credential getAdminCredential() {
+        return password;
+    }
+
+    public String getAdminUsername(){
+        return username;
+    }
+
+    public static SecurityManager getInstance() {
+        return instance;
+    }
+
+    public LoginService getAdminLoginService() {
+        if (adminLoginService != null)
+            return adminLoginService;
+        adminLoginService = new HashLoginService();
+        var store = new UserStore();
+        var credential = SecurityManager.getInstance().getAdminCredential();
+        var admin = SecurityManager.getInstance().getAdminUsername();
+        store.addUser(admin, credential, ROLES);
+        adminLoginService.setUserStore(store);
+        return adminLoginService;
+    }
+
+    public String[] getRoles() {
+        // Returning clone so that no caller can change overall roles
+        return ROLES.clone();
+    }
+}

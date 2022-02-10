b22dd1c078fbb2fa851eabc282f40c129a732a71
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2022-02-10 21:35:37 +0100

    add supprt for retrieving stdout from command execution

--- src/main/java/fundamentals/server/helpers/Bash.java
+++ src/main/java/fundamentals/server/helpers/Bash.java
@@ -1,21 +1,34 @@
 package fundamentals.server.helpers;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Bash {
     private final Runtime runtime = Runtime.getRuntime();
+    private List<String> stdout;
 
     public boolean execute(String[] cmdArray, String[] envArr, File dir) {
         Process process = null;
+        BufferedReader reader = null;
+
         try {
             process = runtime.exec(cmdArray, envArr, dir);
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             process.waitFor();
         } catch (InterruptedException interruptedException) {
             return false;
         } catch (IOException ioException) {
             return false;
+        } finally {
+            stdout = reader.lines().toList();
         }
         return process.exitValue() == 0;
+    }
+
+    public List<String> getStdout() {
+        return stdout;
     }
 }
